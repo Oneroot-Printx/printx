87f04dd8b657b4b805e453afc29d8c5f901225c5
nikhil.jain <nikhilj@nexinfosoft.com>
2022-03-23 21:17:23 +0530

    commit message

---
+++ src/detailModal.js
@@ -0,0 +1,29 @@
+import {Modal,Button} from 'react-bootstrap/'
+
+function DetailModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        
+          <p>
+           {props.description}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="dark" onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+export default DetailModal;
+  
